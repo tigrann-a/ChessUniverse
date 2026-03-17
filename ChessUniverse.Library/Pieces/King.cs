38f74d91f049583c36f459a73fd6d2bde97f539e
School <School@DESKTOP-RR0AV5V>
2026-03-17 16:46:05 +0400

    Creating IsMovePossible logic for King piece

--- ChessUniverse.Library/Pieces/King.cs
+++ ChessUniverse.Library/Pieces/King.cs
@@ -2,13 +2,25 @@
 
 namespace ChessUniverse.Library.Pieces;
 
-public class King(PieceColor color) : Piece(color)
+public class King(PieceColor color) : Piece(color) //primary constructor syntax
 {
 
     public override char GetSymbol() => Color == PieceColor.white ? 'K' : 'k';
 
     public override bool IsMovePossible(Coords start, Coords final, ChessBoard board)
     {
-        return true;
+        int dx = Math.Abs(final.Row - start.Row);
+        int dy = Math.Abs(final.Col - start.Col);
+
+
+
+        if (dx == 0 && dy == 0)
+            return false;
+        else if (dx <= 1 && dy <= 1)
+            return true;
+
+        return false;
+        
+        //return (dx <= 1 && dy <= 1) && !(dx == 0 && dy == 0);
     }
 }
