b6f7be91a53d3ef28b5eb97ec729c0f1d73b4b55
School <School@DESKTOP-RR0AV5V>
2026-03-17 17:40:55 +0400

    Updating King class and making notes about primary constructor

--- ChessUniverse.Library/Pieces/King.cs
+++ ChessUniverse.Library/Pieces/King.cs
@@ -4,15 +4,25 @@ namespace ChessUniverse.Library.Pieces;
 
 public class King(PieceColor color) : Piece(color) //primary constructor syntax
 {
+    // public King(PieceColor color) : base(color) կոնստրուկտոր է
+    // King-ը ստանում է color-ը, այն փոխանցում է Piece-ին և Piece-ը պահում է այդ գույնը իր Color property-ում
+    //                              ^
+    //                              |
+    // ------------------------------------------------------
+    //  public class King : Piece
+    //  {
+    //      public King(PieceColor color) : base(color){}
+    //  }
+    // ------------------------------------------------------
 
+    // override է անում Piece-ում գրված GetSymbol մեթոդը և վերադարձնում է խաղաքարի սիմվոլը
     public override char GetSymbol() => Color == PieceColor.white ? 'K' : 'k';
 
+    // override է անում Piece-ում գրված IsMovePossible մեթոդը և ստուգում է կարող է խաղաքարը շարժվել․ թե ոչ
     public override bool IsMovePossible(Coords start, Coords final, ChessBoard board)
     {
         int dx = Math.Abs(final.Row - start.Row);
         int dy = Math.Abs(final.Col - start.Col);
-
-
 
         if (dx == 0 && dy == 0)
             return false;
